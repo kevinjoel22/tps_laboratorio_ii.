45d210ed01891f7b43a335f1a63e0f4bab62e1af
kevinjoel22 <kevin22.losnades@gmail.com>
2022-04-18 00:12:36 -0300

    segundo commit

---
+++ ejercicio1/ejercicio1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/*Ingresar 5 números por consola, guardándolos en una variable escalar. Luego calcular y mostrar: el valor máximo,
+ el valor mínimo y el promedio*/
+namespace ejercicio1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numero;
+            int maximo = int.MaxValue;
+            int minimo = int.MinValue;
+            int acum = 0;
+            int promedio;
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Ingrese un numero: ");
+                numero = int.Parse(Console.ReadLine());
+                acum += numero;
+
+               if (numero > 0)
+                {
+                    if(minimo < numero)
+                    {
+                        minimo = numero;
+                    }
+
+                    if(maximo > numero)
+                    {
+                        maximo = numero;
+                    }
+                }
+            }
+            promedio = acum / 5;
+            Console.WriteLine("promedio es:"+promedio);
+            Console.WriteLine("el maximo es:"+ minimo);
+            Console.WriteLine("El minimo es: "+maximo);
+        }
+    }
+}
